a7eceac7aa2d6ef99ae8057f91fe57c5127c1768
bogdan-edu <bogdan-education@gmail.com>
2022-01-26 14:10:33 +0200

    add

---
+++ simple_python/numbers.py
@@ -0,0 +1,20 @@
+num1 = 16
+num2 = 6
+
+result = num1 + num2
+print(result)
+
+result = num1 - num2
+print(result)
+
+result = num1 * num2
+print(result)
+
+result = num1 / num2
+print(result)
+
+result = num1 ** 2
+print(result)
+
+result = num1 ** 0.5
+print(result)
---
+++ simple_python/texts.py
@@ -0,0 +1,8 @@
+var1 = "Привіт! "
+var2 = "8 клас"
+
+result = var1 + var2
+print(result)
+
+result = var1 * 8
+print(result)
