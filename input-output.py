7d075ca8a86ffe24d0f303858c0a85b79ff5838b
Bogdan Kalnybolotchuk <45575774+bogdan-edu@users.noreply.github.com>
2022-01-31 09:10:50 +0200

    Create input-output.py

---
+++ input-output.py
@@ -0,0 +1,6 @@
+user_name = input("Як тебе звати? ")
+a = float(input("Введіть сторону a = "))
+b = float(input("Введіть сторону b = ")) 
+c = float(input("Введіть сторону c = "))
+p = a + b + c
+print(user_name, ", периметр трикутника p =", p)
