bcad5f3257dc976ef3a7c6230eec7e350b9b42a1
Bogdan Kalnybolotchuk <bogdan.kalnybolotchuk@gmail.com>
2022-02-09 14:02:15 +0200

    add if else

---
+++ if-else/if-else-statement.py
@@ -0,0 +1,15 @@
+user_name = input("Як тебе звати? ")
+user_age = input("Скільки тобі років? ")
+user_age = int(user_age)
+
+if user_age > 14:
+    print(user_name, ", ти старший за нас")
+    print(user_name, ", ти старший за нас")
+elif user_age == 14:
+    print(user_name, ", ти наш однолітка")
+    print(user_name, ", ти наш однолітка")
+else:
+    print(user_name, ", ти молодший за нас")
+    print(user_name, ", ти молодший за нас")
+
+print("The End ;)")
